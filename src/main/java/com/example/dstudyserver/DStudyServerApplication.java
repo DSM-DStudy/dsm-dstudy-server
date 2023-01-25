1a9f6fd0e2c9b8e1046f0cc4363a0e4346d04cd2
wheeeung <whee050916@gmail.com>
2023-01-25 20:58:26 +0900

    ⚙️ :: project setting

---
+++ src/main/java/com/example/dstudyserver/DStudyServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.dstudyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DStudyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DStudyServerApplication.class, args);
+    }
+
+}
