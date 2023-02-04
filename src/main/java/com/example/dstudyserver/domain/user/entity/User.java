b941dc9b1f06caabb1a9eae85a9e0d9e317add6e
wheeeung <whee050916@gmail.com>
2023-02-04 13:36:08 +0900

    📑 :: add user entity

---
+++ src/main/java/com/example/dstudyserver/domain/user/entity/User.java
@@ -0,0 +1,30 @@
+package com.example.dstudyserver.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+    @Column
+    private String introduction;
+
+    @Column
+    private String image;
+}
