9b63a7d32cc5c594e0184e3c5683fab731dc97c8
wheeeung <whee050916@gmail.com>
2023-02-18 20:44:34 +0900

    feat:: add WebMvcConfig

---
+++ src/main/java/com/example/dstudyserver/global/config/WebMvcConfig.java
@@ -0,0 +1,15 @@
+package com.example.dstudyserver.global.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .exposedHeaders("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
