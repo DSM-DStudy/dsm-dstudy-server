3d16d659a05ca672f7f6f423f4955e940ed7b753
wheeeung <whee050916@gmail.com>
2023-02-21 10:15:20 +0900

    fix:: rename join

--- src/main/java/com/example/dstudyserver/domain/join/entity/Entry.java
+++ src/main/java/com/example/dstudyserver/domain/join/entity/Entry.java
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class Join {
+public class Entry {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
