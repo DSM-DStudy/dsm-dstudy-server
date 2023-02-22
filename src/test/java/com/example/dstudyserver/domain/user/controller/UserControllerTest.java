1c946879de490c92f3f376569bbf20d9a61910b2
wheeeung <whee050916@gmail.com>
2023-02-22 20:50:10 +0900

    test:: add editProfile controller test

--- src/test/java/com/example/dstudyserver/domain/user/controller/UserControllerTest.java
+++ src/test/java/com/example/dstudyserver/domain/user/controller/UserControllerTest.java
@@ -1,10 +1,12 @@
 package com.example.dstudyserver.domain.user.controller;
 
+import com.example.dstudyserver.domain.user.controller.dto.request.EditProfileRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -25,5 +27,23 @@ public class UserControllerTest {
                 MockMvcRequestBuilders.get("/mypage")
                         .header("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0QGdtYWlsLmNvbSIsImlhdCI6MTY3NzA2NjE2MSwiZXhwIjoxNjc3MDY5NzYxfQ.6XIlUqaZI0zh8ARJX82Tzk491wuT9Ps1QACUPurkNFU")
         ).andExpect(status().isOk());
+    }
+
+    @Test
+    public void editProfile() throws Exception{
+        String introduction = "abcdefg";
+        String image = "asdfafasfaasfasfsadfsafasf";
+
+        EditProfileRequest request = new EditProfileRequest(introduction, image);
+        String json = objectMapper.writeValueAsString(request);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.patch("/mypage")
+                        .header("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0QGdtYWlsLmNvbSIsImlhdCI6MTY3NzA2NjE2MSwiZXhwIjoxNjc3MDY5NzYxfQ.6XIlUqaZI0zh8ARJX82Tzk491wuT9Ps1QACUPurkNFU")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk());
+
     }
 }
