f567d0f9ea33a144fed229c0daf9b7aad6375f59
wheeeung <whee050916@gmail.com>
2023-02-22 20:38:20 +0900

    test:: add login controller test

--- src/test/java/com/example/dstudyserver/domain/user/controller/AuthControllerTest.java
+++ src/test/java/com/example/dstudyserver/domain/user/controller/AuthControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.dstudyserver.domain.user.controller;
 
+import com.example.dstudyserver.domain.user.controller.dto.request.LoginRequest;
 import com.example.dstudyserver.domain.user.controller.dto.request.SignupRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
@@ -31,6 +32,22 @@ public class AuthControllerTest {
 
         mockMvc.perform(
                 MockMvcRequestBuilders.post("/signup")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk());
+    }
+
+    @Test
+    public void login() throws Exception{
+        String email = "testcode@gmail.com";
+        String password = "1234";
+
+        LoginRequest request = new LoginRequest(email, password);
+        String json = objectMapper.writeValueAsString(request);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/login")
                         .content(json)
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON)
