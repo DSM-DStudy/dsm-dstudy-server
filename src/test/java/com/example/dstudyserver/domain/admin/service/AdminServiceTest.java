2b01b0f60416f37b5009b6029fad563510cc4fa2
wheeeung <whee050916@gmail.com>
2023-02-23 16:58:50 +0900

    test:: add admin test

--- src/test/java/com/example/dstudyserver/domain/admin/service/AdminServiceTest.java
+++ src/test/java/com/example/dstudyserver/domain/admin/service/AdminServiceTest.java
@@ -1,6 +1,7 @@
 package com.example.dstudyserver.domain.admin.service;
 
 import com.example.dstudyserver.domain.admin.controller.dto.response.EntryResponse;
+import com.example.dstudyserver.domain.admin.controller.dto.response.UserResponse;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,4 +21,18 @@ public class AdminServiceTest {
         Assertions.assertEquals(list.size(), 0);
     }
 
+    @Test
+    public void accept(){
+        boolean accept = true;
+        int entry_id = 1;
+
+        adminService.accept(accept, entry_id);
+    }
+
+    @Test
+    public void userList(){
+        List<UserResponse> list = adminService.userList();
+
+        Assertions.assertEquals(list.size(), 2);
+    }
 }
