db1db281cde557a9fa9dec3de163bf1b05a00270
wheeeung <whee050916@gmail.com>
2023-03-02 19:30:56 +0900

    feat:: add security user service

---
+++ src/main/java/com/example/dstudyserver/global/security/SecurityUserService.java
@@ -0,0 +1,29 @@
+package com.example.dstudyserver.global.security;
+
+import com.example.dstudyserver.domain.user.entity.User;
+import com.example.dstudyserver.domain.user.exception.UserNotFoundException;
+import com.example.dstudyserver.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class SecurityUserService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username).orElseThrow(UserNotFoundException::new);
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(user.getRole().name()));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorities);
+    }
+}
