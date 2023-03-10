0de39121df049ae277e3ca23ecd5b5a4d90921ae
wheeeung <whee050916@gmail.com>
2023-03-10 11:17:32 +0900

    test:: delete unused import

--- src/test/java/com/example/dstudyserver/domain/tip/service/TipServiceTest.java
+++ src/test/java/com/example/dstudyserver/domain/tip/service/TipServiceTest.java
@@ -12,7 +12,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.List;
 
