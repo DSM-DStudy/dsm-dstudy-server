eebc0e8ab49e469f2d537d5b9aa06223736561e6
wjdtn0829 <oh050211@gmail.com>
2023-03-15 22:00:56 +0900

    feat:: add lounge

--- src/main/java/com/example/dstudyserver/domain/lounge/repository/LoungeRepository.java
+++ src/main/java/com/example/dstudyserver/domain/lounge/repository/LoungeRepository.java
@@ -1,7 +1,11 @@
 package com.example.dstudyserver.domain.lounge.repository;
 
 import com.example.dstudyserver.domain.lounge.entity.Lounge;
+import com.example.dstudyserver.domain.tips.entity.Tips;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LoungeRepository extends JpaRepository <Lounge, Integer> {
+    List<Lounge> findByTitleContaining(String keyword);
 }
