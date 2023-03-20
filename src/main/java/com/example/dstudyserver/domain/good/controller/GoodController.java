be497a0b7272d9d4874587b07cbd2339f2dfa903
wheeeung <whee050916@gmail.com>
2023-03-20 09:16:29 +0900

    feat:: add good operation

--- src/main/java/com/example/dstudyserver/domain/good/controller/GoodController.java
+++ src/main/java/com/example/dstudyserver/domain/good/controller/GoodController.java
@@ -1,6 +1,7 @@
 package com.example.dstudyserver.domain.good.controller;
 
 import com.example.dstudyserver.domain.good.service.GoodService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,11 +14,13 @@ public class GoodController {
     private final GoodService goodService;
 
     @PostMapping("/like/{study_id}")
+    @Operation(summary = "좋아요 추가")
     public void insertGood(@PathVariable int study_id){
         goodService.insertGood(study_id);
     }
 
     @DeleteMapping("/like/{study_id}")
+    @Operation(summary = "좋아요 삭제")
     public void deleteGood(@PathVariable int study_id){
         goodService.deleteGood(study_id);
     }
