0fdbab0964ea8bd4e8301b85b0c8c65b4d0d4458
wheeeung <whee050916@gmail.com>
2023-03-20 09:15:20 +0900

    feat:: add comment operation

--- src/main/java/com/example/dstudyserver/domain/comment/controller/CommentController.java
+++ src/main/java/com/example/dstudyserver/domain/comment/controller/CommentController.java
@@ -3,6 +3,7 @@ package com.example.dstudyserver.domain.comment.controller;
 import com.example.dstudyserver.domain.comment.controller.dto.request.CommentRequest;
 import com.example.dstudyserver.domain.comment.controller.dto.response.CommentResponse;
 import com.example.dstudyserver.domain.comment.service.CommentService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -16,16 +17,19 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping("/comment/{study_id}")
+    @Operation(summary = "댓글 쓰기")
     public CommentResponse save(@Valid @RequestBody CommentRequest request, @PathVariable int study_id){
         return commentService.save(request, study_id);
     }
 
     @DeleteMapping("/comment/{comment_id}")
+    @Operation(summary = "댓글 삭제")
     public void delete(@PathVariable int comment_id){
         commentService.delete(comment_id);
     }
 
     @GetMapping("/comment/list")
+    @Operation(summary = "댓글 리스트")
     public List<CommentResponse> commentList(){
         return commentService.commentList();
     }
