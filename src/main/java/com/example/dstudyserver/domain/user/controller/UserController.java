55b3259089efb8eaf4dd692481f033821c4f35d3
wheeeung <whee050916@gmail.com>
2023-03-21 09:52:15 +0900

    feat:: add operation user controller

--- src/main/java/com/example/dstudyserver/domain/user/controller/UserController.java
+++ src/main/java/com/example/dstudyserver/domain/user/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.dstudyserver.domain.user.controller;
 import com.example.dstudyserver.domain.user.controller.dto.request.EditProfileRequest;
 import com.example.dstudyserver.domain.user.controller.dto.response.UserResponse;
 import com.example.dstudyserver.domain.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -15,11 +16,13 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/mypage")
+    @Operation(summary = "유저 정보 보기")
     public UserResponse getUser(){
         return userService.getUser();
     }
 
     @PatchMapping("/mypage")
+    @Operation(summary = "프로필 수정")
     public void editProfile(@RequestBody EditProfileRequest request){
         userService.editProfile(request);
     }
