0d1e1531f8b85be0e5e2000795c22bfc2ac9f939
wheeeung <whee050916@gmail.com>
2023-03-21 09:48:50 +0900

    feat:: add operation

--- src/main/java/com/example/dstudyserver/domain/study/controller/StudyController.java
+++ src/main/java/com/example/dstudyserver/domain/study/controller/StudyController.java
@@ -3,6 +3,7 @@ package com.example.dstudyserver.domain.study.controller;
 import com.example.dstudyserver.domain.study.controller.dto.request.StudyRequest;
 import com.example.dstudyserver.domain.study.controller.dto.response.StudyResponse;
 import com.example.dstudyserver.domain.study.service.StudyService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -16,31 +17,37 @@ public class StudyController {
     private final StudyService studyService;
 
     @PostMapping("/study")
+    @Operation(summary = "스터디 생성")
     public StudyResponse save(@Valid @RequestBody StudyRequest request) {
         return studyService.save(request);
     }
 
     @DeleteMapping("study/{study_id}")
+    @Operation(summary = "스터디 삭제")
     public void delete(@PathVariable int study_id){
         studyService.delete(study_id);
     }
 
     @GetMapping("/study/{study_id}")
+    @Operation(summary = "스터디 보기")
     public StudyResponse getStudy(@PathVariable int study_id){
         return studyService.getStudy(study_id);
     }
 
     @GetMapping("/study/list")
+    @Operation(summary = "스터디 리스트")
     public List<StudyResponse> studyList(){
         return studyService.studyList();
     }
 
     @GetMapping("/study")
+    @Operation(summary = "스터디 검색")
     public List<StudyResponse> search(@RequestParam("keyword") String keyword){
         return studyService.search(keyword);
     }
 
     @PatchMapping("/study/{study_id}")
+    @Operation(summary = "스터디 수정")
     public void editStudy(@PathVariable int study_id, @Valid @RequestBody StudyRequest request){
         studyService.editStudy(study_id, request);
     }
--- src/main/java/com/example/dstudyserver/domain/tips/controller/TipsController.java
+++ src/main/java/com/example/dstudyserver/domain/tips/controller/TipsController.java
@@ -3,6 +3,7 @@ package com.example.dstudyserver.domain.tips.controller;
 import com.example.dstudyserver.domain.tips.controller.dto.request.TipsRequest;
 import com.example.dstudyserver.domain.tips.controller.dto.response.TipsResponse;
 import com.example.dstudyserver.domain.tips.service.TipsService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -15,26 +16,31 @@ public class TipsController {
     private final TipsService tipsService;
 
     @PostMapping("/tip")
+    @Operation(summary = "꿀팁 게시판 글 저장")
     public TipsResponse save(@Valid @RequestBody TipsRequest request){
         return tipsService.save(request);
     }
 
     @DeleteMapping("/tip/{tip_id}")
+    @Operation(summary = "꿀팁 게시판 글 삭제")
     public void delete(@PathVariable int tip_id){
         tipsService.delete(tip_id);
     }
 
     @GetMapping("/tip/{tip_id}")
+    @Operation(summary = "꿀팁 게시판 글 보기")
     public TipsResponse getTips(@PathVariable int tip_id){
         return tipsService.getTips(tip_id);
     }
 
     @GetMapping("/tip/list")
+    @Operation(summary = "꿀팁 게시판 글 리스트")
     public List<TipsResponse> tipsList(){
         return tipsService.tipsList();
     }
 
     @GetMapping("/tip")
+    @Operation(summary = "꿀팁 게시판 검색")
     public List<TipsResponse> search(@RequestParam("keyword") String keyword){
         return tipsService.search(keyword);
     }
